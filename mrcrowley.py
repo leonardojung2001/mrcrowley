80fc9807431a5e7aefcb28ec17834642a647b78f
Leonardo <45762153+leonardojung2001@users.noreply.github.com>
2022-06-22 13:25:44 -0300

    Update mrcrowley.py

--- mrcrowley.py
+++ mrcrowley.py
@@ -3,18 +3,31 @@ import requests
 
 subpage_list = ['admin.php', 'wp-admin', 'control-panel', 'admin', 'shop/Admin.php']
 
-url = "https://roldf.org"
 
+def main():
+  print("Welcome to MrCrowley a tool desing to crawl into webpages and try to find admin pages\n")
+  url =  input("Type the url ex[https://test.com]:")
+  crawl(url)
 
-for subpage in subpage_list:
-	r = requests.head(url + "/" + subpage)
+def crawl(url):
+  
+  try:
+    
+    for subpage in subpage_list:
+  	  r = requests.head(url + "/" + subpage)
 
-	if r.status_code == 200: #check if the site responds 200 
-		print("Found admin page:\n")
-		print(url + "/" + subpage)
-		exit()
+  	  if r.status_code == 200: #check if the site responds 200 
+  		  print("Found admin page:\n")
+  		  print(url + "/" + subpage)
+	  	  exit()
 	
-print("Something went wrong or not found admin page!")
+  except SSLCertVerificationError:
+    print("You got a problem with the SSL certificate on provided page!")
+    exit()
 
+
+
+if __name__ == "__main__":
+  main()
 
 
